e9c1519b071c58321871743a28115f6b332008d5
williancozzi <williancozzi@gmail.com>
2020-07-21 19:34:46 -0300

    adjusting the search and the html

--- js/script.js
+++ js/script.js
@@ -1,18 +1,48 @@
+//Challenge to-do:
+//(✓)Mostrar o loading enquanto carrega os dados
+//    e inabilita o input Text só enquanto carrega;
+//(✓)Na carga inicial da aplicação, obter os dados da api;
+//(✓)Carregar os dados dos usuários em um array;
+//(✓)Permitir a filtragem de usuários através de um input com
+//    interação do usuário;
+//(✓)O usuário poderá filtrar dados quando digitar pelo menos um
+//    caractere no input;
+//(✓)O usuário poderá filtrar os dados tanto digitando "Enter"
+//    quanto clicando no botão;
+//(✓)Montar dois painéis;
+//()No painel da esquerda, listar os usuários filtrados;
+//()No painel da esquerda, mostrar quantos usuários foram filtrados;
+//()No painel da direita, calcular e mostrar algumas estatísticas
+//    sobre esses usuários;
+
 "use strict";
 
 let allPeople = [];
 let filteredPeople = [];
+
+let tabPeople = document.querySelector("#tab-people");
+let tabStatistics = document.querySelector("#tab-statistics");
 
 let inputText = document.querySelector("#input-text");
 let loading = document.querySelector(".loading");
 let btnSearch = document.querySelector("#btn-search");
 let lblSearch = document.querySelector("#lbl-search");
 
+let txtUsers = document.querySelector("#text-users");
+let txtStatistics = document.querySelector("#text-statistics");
+
 window.addEventListener("load", () => {
   loadData();
 });
 
-inputText.addEventListener("keyup", checkInput);
+inputText.addEventListener("keyup", function events(e) {
+  checkInput();
+
+  if (inputText.value.length > 0 && e.keyCode === 13) {
+    doSearch();
+  }
+});
+
 inputText.addEventListener("focus", onFocus);
 inputText.addEventListener("focusout", onFocusOut);
 btnSearch.addEventListener("click", doSearch);
@@ -51,7 +81,41 @@ function doSearch() {
       .includes(inputText.value.toLowerCase());
   });
 
-  console.log(filteredPeople);
+  render();
+  console.log("entrei aqui no render()");
+}
+
+function render() {
+  renderFilteredPeople();
+}
+
+function renderFilteredPeople() {
+  let peopleHTML = "<div>";
+
+  filteredPeople.forEach((person) => {
+    const { picture, name, age } = person;
+
+    const personHTML = `
+    <div class='person'>
+      <div>
+        <img src="${picture}" alt="${name}">
+      </div>
+      <div>
+        <ul>
+          <li>${name}, </li>
+          <li>${age} anos</li>
+        <ul>
+      </div>
+    </div>
+    `;
+
+    peopleHTML += personHTML;
+  });
+
+  peopleHTML += "</div>";
+
+  tabPeople.innerHTML = peopleHTML;
+  updateHtml(peopleHTML.length);
 }
 
 function isLoaded() {
@@ -67,4 +131,14 @@ function onFocusOut() {
   //prettier-ignore
   if (!inputText.value) 
     lblSearch.classList.remove("active");
+}
+
+function updateHtml(users) {
+  if (users === 0) {
+    txtUsers.textContent = "Nenhum usuário filtrado";
+    txtStatistics.textContent = "Nada a ser exibido";
+  } else {
+    tabPeople.innerHTML = "x usuário(s) encontrado(s)";
+    tabStatistics.innerHTML = "Estatísticas";
+  }
 }
